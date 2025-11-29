0b9a8c99d79731f9b1f06fe7f87a3aca1a117e3f
Davidsb04 <caisilvasb756@hotmail.com>
2025-11-29 17:37:00 -0300

    inicializando classes Musica e Playlist

---
+++ Musica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerMusical
+{
+    internal class Musica
+    {
+        private string titulo;
+        private string artista;
+        private string genero;
+        private int duracao;
+        private string chave;
+
+        public Musica(string titulo, string artista, string genero, int duracao)
+        {
+            this.titulo = titulo;
+            this.artista = artista;
+            this.genero = genero;
+            this.duracao = duracao;
+            chave = titulo + artista;
+        }
+    }
+}
---
+++ Playlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerMusical
+{
+    internal class Playlist
+    {
+        private string nome;
+        private ListaDupla playlist;
+
+        public Playlist(string nome)
+        {
+            this.nome = nome;
+            playlist = new ListaDupla();
+        }
+
+    }
+}
